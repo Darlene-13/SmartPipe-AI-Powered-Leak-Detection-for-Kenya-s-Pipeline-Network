93a1393c26ac4349e20b50bc95ad75611e98268c
Darlene-13 <darlenewendie@gmail.com>
2026-01-18 15:46:24 +0300

    Initialized springboot project..

---
+++ springboot-backend/hydraulic-modelling/src/main/java/darlene/github/com/hydraulicmodelling/HydraulicModellingApplication.java
@@ -0,0 +1,13 @@
+package darlene.github.com.hydraulicmodelling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydraulicModellingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HydraulicModellingApplication.class, args);
+	}
+
+}
---
+++ springboot-backend/hydraulic-modelling/src/test/java/darlene/github/com/hydraulicmodelling/HydraulicModellingApplicationTests.java
@@ -0,0 +1,13 @@
+package darlene.github.com.hydraulicmodelling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HydraulicModellingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
