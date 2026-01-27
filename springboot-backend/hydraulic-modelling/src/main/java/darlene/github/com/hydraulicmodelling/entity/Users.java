fbc6041d555c2b5745c60f6c000cd1da1506f9b1
Darlene-13 <darlenewendie@gmail.com>
2026-01-27 16:20:22 +0300

    Users model

---
+++ springboot-backend/hydraulic-modelling/src/main/java/darlene/github/com/hydraulicmodelling/entity/Users.java
@@ -0,0 +1,21 @@
+package darlene.github.com.hydraulicmodelling;
+
+import jakarta.persistence*;
+import lombok data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsContstructor;
+import org.hibernate.annotations.CreationTimeStamp;
+
+
+import java.LocalDateTime;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "users")
+
+
+public class Users{
+
+}
