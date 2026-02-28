7d9a13dcf812ee42009b95ee0d2a9c4e53ecd4bf
Darlene-13 <darlenenasimiyu@gmail.com>
2026-02-28 16:32:46 +0300

    Initial commit

---
+++ LeakDetectionApplication/src/main/java/io/github/darlene/leakdetectionapplication/LeakDetectionApplication.java
@@ -0,0 +1,13 @@
+package io.github.darlene.leakdetectionapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeakDetectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeakDetectionApplication.class, args);
+    }
+
+}
---
+++ LeakDetectionApplication/src/test/java/io/github/darlene/leakdetectionapplication/LeakDetectionApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.darlene.leakdetectionapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeakDetectionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
