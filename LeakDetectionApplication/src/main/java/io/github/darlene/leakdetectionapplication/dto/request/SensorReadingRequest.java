ba36bd13cb5e8640c57464db0ef9deb666bc04e0
Darlene-13 <darlenenasimiyu@gmail.com>
2026-03-27 08:08:41 +0300

    Fix: reduced payload

--- LeakDetectionApplication/src/main/java/io/github/darlene/leakdetectionapplication/dto/request/SensorReadingRequest.java
+++ LeakDetectionApplication/src/main/java/io/github/darlene/leakdetectionapplication/dto/request/SensorReadingRequest.java
@@ -26,36 +26,36 @@ import java.time.LocalDateTime;
 
 public class SensorReadingRequest {
 
-    @JsonProperty("device_id")
+    @JsonProperty("id")
     @NotBlank(message = "Device Id cannot be blank")
     private String deviceId;
 
-    @JsonProperty("timestamp")
+    @JsonProperty("ts")
     @NotNull(message = "Every Reading should have a timestamp")
     private LocalDateTime timestamp;
 
-    @JsonProperty("node_a_pressure")
+    @JsonProperty("pa")
     @NotNull(message = "Node A pressure reading is required")
     @Positive(message = "Node A pressure must be positive")
     @DecimalMin(value = "50000.0", message = "Pressure below operational minimum")
     @DecimalMax(value = "1000000.0", message = "Pressure exceeds operational maximum")
     private Double nodeAPressure;
 
-    @JsonProperty("node_b_pressure")
+    @JsonProperty("pb")
     @NotNull(message = "Node B pressure reading is required")
     @Positive(message = "Node B pressure must be positive")
     @DecimalMin(value = "50000.0", message = "Pressure below operational minimum")
     @DecimalMax(value = "1000000.0", message = "Pressure exceeds operational maximum")
     private Double nodeBPressure;
 
-    @JsonProperty("node_c_pressure")
+    @JsonProperty("pc")
     @NotNull(message = "Node C pressure reading is required")
     @Positive (message = "Node C pressure must be positive")
     @DecimalMin(value = "50000.0", message = "Pressure below operational minimum")
     @DecimalMax(value = "1000000.0", message = "Pressure exceeds operational maximum")
     private Double nodeCPressure;
 
-    @JsonProperty("flow_velocity")
+    @JsonProperty("fv")
     @NotNull
     @DecimalMin(value = "0.5", message = "Flow velocity below minimum operational threshold")
     @DecimalMax(value = "10.0", message = "Flow velocity exceeds maximum operational threshold")
@@ -63,7 +63,7 @@ public class SensorReadingRequest {
     @DecimalMax("10.0")
     private Double flowVelocity;
 
-    @JsonProperty("scenario")
+    @JsonProperty("sc")
     private String scenario;
 
 }
