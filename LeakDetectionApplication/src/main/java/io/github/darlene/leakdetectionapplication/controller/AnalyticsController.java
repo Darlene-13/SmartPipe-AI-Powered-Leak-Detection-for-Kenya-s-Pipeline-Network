997f87d767e3505b0c1b391fc57ac011098fe005
Darlene-13 <darlenewendie@gmail.com>
2026-04-07 18:39:59 +0300

    Added analytics controller

--- LeakDetectionApplication/src/main/java/io/github/darlene/leakdetectionapplication/controller/AnalyticsController.java
+++ LeakDetectionApplication/src/main/java/io/github/darlene/leakdetectionapplication/controller/AnalyticsController.java
@@ -3,24 +3,20 @@ package io.github.darlene.leakdetectionapplication.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.http.ResponseEntity;
-import org.springframework.data.domain.Page;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.RequiredArgsConstructor;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 
-import io.github.darlene.leakdetectionapplication.service.AlertService;
-import io.github.darlene.leakdetectionapplication.service.LatencyTrackingService;
-import io.github.darlene.leakdetectionapplication.service.SensorReadingService;
+import io.github.darlene.leakdetectionapplication.service.AnalyticsService;
 
-import io.github.darlene.leakdetectionapplication.dto.response.AnalyticsServiceResponse;
+import io.github.darlene.leakdetectionapplication.dto.response.AnalyticsSummaryResponse;
 import io.github.darlene.leakdetectionapplication.dto.response.LatencyStatsResponse;
 
-import java.util.LocalDateTime;
+import java.time.LocalDateTime;
 import java.util.Map;
 
 @RestController
@@ -28,18 +24,40 @@ import java.util.Map;
 @RequiredArgsConstructor
 @Tag(name = "Analytics")
 @RequestMapping("/api/analytics")
-public class AnalyticsController{
+public class AnalyticsController {
 
-    private final AlertService alertService;
-    private final LatencyTrackingService latencyTrackingService;
-    private final SensorReadingService sensorReadingService;
+    private final AnalyticsService analyticsService;
 
     @GetMapping("/summary")
-    public ResponseEntity<AnalyticsSummaryResponse> getAnalyticsSummary(@RequestParam @DateTimeFormat from,
-                                              @RequestParam @DateTimeFormat to){
-        log.info("Fetching analytics summary from {} to {}");
-        AlertService alertService = alertService.getAnalyticsSummary(from, to);
-        return ResponseEntity.ok(summary)
+    public ResponseEntity<AnalyticsSummaryResponse> getAnalyticsSummary(
+            @RequestParam
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime from,
+
+            @RequestParam
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime to) {
+
+        log.info("Fetching analytics summary...");
+        AnalyticsSummaryResponse analytics = analyticsService.getSummary(from, to);
+        return ResponseEntity.ok(analytics);
     }
 
+    @GetMapping("/latency/stats")
+    public ResponseEntity<LatencyStatsResponse> getLatencyStatsResponse() {
+        log.info("Fetching latency stats...");
+        LatencyStatsResponse stats = analyticsService.getLatencyStats();
+        return ResponseEntity.ok(stats);
+    }
+
+    @GetMapping("/fault-distribution")
+    public ResponseEntity<Map<String, Long>> getFaultListDistribution(
+            @RequestParam
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime from,
+
+            @RequestParam
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime to) {
+
+        log.info("Fetching fault distribution...");
+        Map<String, Long> distribution = analyticsService.getFaultDistribution(from, to);
+        return ResponseEntity.ok(distribution);
+    }
 }
