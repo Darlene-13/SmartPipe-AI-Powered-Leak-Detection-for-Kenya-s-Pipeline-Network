791009a8142d4907c02a7c16a7ee84c245371d13
Darlene-13 <darlenewendie@gmail.com>
2026-04-08 05:50:27 +0300

    added simulation controller

--- LeakDetectionApplication/src/main/java/io/github/darlene/leakdetectionapplication/controller/SimulationController.java
+++ LeakDetectionApplication/src/main/java/io/github/darlene/leakdetectionapplication/controller/SimulationController.java
@@ -1,45 +1,40 @@
 package io.github.darlene.leakdetectionapplication.controller;
 
-
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.http.ResponseEntity;
 
-import org.springframework.format.annotation.DateTimeFormat;
+import jakarta.validation.Valid;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
 
-
 import io.github.darlene.leakdetectionapplication.service.ProcessingService;
 import io.github.darlene.leakdetectionapplication.service.CacheService;
 
 import io.github.darlene.leakdetectionapplication.dto.request.SimulationRequest;
-import io.github.darlene.leakdetectionapplication.dto.response.FaultALertResponse;
+import io.github.darlene.leakdetectionapplication.dto.response.FaultAlertResponse;
 
 import java.util.List;
-
 
 @RestController
 @RequestMapping("/api/simulate")
 @RequiredArgsConstructor
-@slf4j
+@Slf4j
 @Tag(name = "Simulation")
-
-public class SimulationController{
+public class SimulationController {
 
     private final ProcessingService processingService;
     private final CacheService cacheService;
 
     @GetMapping("/scenarios")
-    public ResponseEntity<List<>> getAvailableScenarios(){
+    public ResponseEntity<List<String>> getAvailableScenarios() {
         log.info("Fetching available scenarios");
         return ResponseEntity.ok(List.of(
                 "NORMAL_BASELINE",
@@ -52,4 +47,19 @@ public class SimulationController{
         ));
     }
 
+    @PostMapping("/scenario/{scenarioName}")
+    public ResponseEntity<FaultAlertResponse> simulateScenario(@PathVariable String scenarioName) {
+        log.info("Simulating scenario: {}", scenarioName);
+        cacheService.clearAllPredictions();
+        FaultAlertResponse alertResponse = processingService.simulateScenario(scenarioName);
+        return ResponseEntity.ok(alertResponse);
+    }
+
+    @PostMapping("/inject-fault")
+    public ResponseEntity<FaultAlertResponse> injectFault(@Valid @RequestBody SimulationRequest request) {
+        log.info("Injecting fault: {} severity: {}", request.getFaultType(), request.getSeverity());
+        cacheService.clearAllPredictions();
+        FaultAlertResponse alertResponse = processingService.injectFault(request);
+        return ResponseEntity.ok(alertResponse);
+    }
 }
