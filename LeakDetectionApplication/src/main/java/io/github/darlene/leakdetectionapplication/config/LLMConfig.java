8e90f104740eef7a3d5f60ffb151bcd1be0aa2cc
Darlene-13 <darlenewendie@gmail.com>
2026-04-10 20:45:14 +0300

    replace ollam config with grok config

--- LeakDetectionApplication/src/main/java/io/github/darlene/leakdetectionapplication/config/LLMConfig.java
+++ LeakDetectionApplication/src/main/java/io/github/darlene/leakdetectionapplication/config/LLMConfig.java
@@ -1,15 +1,15 @@
 package io.github.darlene.leakdetectionapplication.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class OllamaConfig {
+public class LLMConfig {
 
     @Bean
-    public ChatClient.Builder chatClientBuilder(
-            org.springframework.ai.ollama.OllamaChatModel ollamaChatModel) {
-        return ChatClient.builder(ollamaChatModel);
+    public ChatClient.Builder chatClientBuilder(OpenAiChatModel openAiChatModel) {
+        return ChatClient.builder(openAiChatModel);
     }
 }
