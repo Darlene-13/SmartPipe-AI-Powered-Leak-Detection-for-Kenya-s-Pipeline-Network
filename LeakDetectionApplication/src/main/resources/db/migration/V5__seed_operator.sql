57f7b4cd6c4925212a0e080dacc9ae1e6fbbc260
Darlene-13 <darlenenasimiyu@gmail.com>
2026-04-11 20:06:36 +0300

    Operator flyway migrations

--- LeakDetectionApplication/src/main/resources/db/migration/V5__seed_operator.sql
+++ LeakDetectionApplication/src/main/resources/db/migration/V5__seed_operator.sql
@@ -1,4 +1,4 @@
 -- Seed default operator account (password: Admin@1234)
 INSERT INTO users (first_name, last_name, username, password, role)
-VALUES ('Darlene', 'Wendy', 'darlene', '$2b$10$OJs5gV1lvICbbgVlStnZ.O.ienV05dNd0oz80.DrUYTvZouQe/tyS', 'ROLE_OPERATOR')
+VALUES ('Darlene', 'Wendy', 'darlene', '$2a$10$OJs5gV1lvICbbgVlStnZ.O.ienV05dNd0oz80.DrUYTvZouQe/tyS', 'ROLE_OPERATOR')
     ON CONFLICT (username) DO NOTHING;
