84bff3a6724bc5e08317661046d246d0c02288a2
Darlene-13 <darlenewendie@gmail.com>
2026-05-09 20:19:05 +0300

    mqtt handler header file

--- firmware/src/mqtt_handler.h
+++ firmware/src/mqtt_handler.h
@@ -1,1 +1,39 @@
 #pragma once
+
+#include <Arduino.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/queue.h"
+#include <ArduinoJson.h>
+#include <MycilaMQTT.h>
+#include "system_types.h"
+#include <string>
+#undef MQTT  // guard against macro collision with MycilaMQTT.h
+
+
+
+class MqttHandler{
+
+    public:
+
+        MqttHandler(QueueHandle_t ledQueue);
+
+        void initMqtt();
+        bool connectMqtt();
+        void publishSensorReading(SensorReading reading);
+        void publishHeartBeat();
+        void maintainConnection();
+
+
+    private:
+        Mycila::MQTT _mqttClient;
+
+        uint32_t _publishCount = 0;
+        uint32_t _errorCount = 0;
+
+        QueueHandle_t _ledCommandQueue;
+
+        void _handleMessage(const std::string& topic, const std::string& payload);
+
+
+
+};
--- firmware/src/system_state.h
+++ firmware/src/system_state.h
@@ -1,1 +1,15 @@
 #pragma once
+#include <Arduino.h>
+#include "wifi_manager.h"
+// Struct to group related data together for mqtt handler to use.
+struct SensorReading{
+
+    String deviceId;
+    double nodeAPressure;
+    double nodeBPressure;
+    double nodeCPressure;
+    double velocityA;
+    double velocityB;
+    double velocityC;
+    
+};
