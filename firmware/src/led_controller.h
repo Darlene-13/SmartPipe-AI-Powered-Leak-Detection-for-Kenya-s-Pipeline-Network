c1973317c01d2f24a1012a9f905853b37514e2cd
Darlene-13 <darlenewendie@gmail.com>
2026-05-11 23:20:28 +0300

    added led configuration

--- firmware/src/led_controller.h
+++ firmware/src/led_controller.h
@@ -1,1 +1,15 @@
 #pragma once
+
+#include <Arduino.h>
+#include "config.h"
+
+
+class LedController {
+
+    public:
+        void initLed();
+        void setColor(const char* color);
+
+    private:
+        char currentColor[32]; // Assuming a maximum length for the color string
+};
