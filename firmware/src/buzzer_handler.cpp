5be1c685298550f3a42743850066b975f8af3d01
Darlene-13 <darlenewendie@gmail.com>
2026-05-13 05:26:15 +0300

    implemeted buzzer handler

---
+++ firmware/src/buzzer_handler.cpp
@@ -0,0 +1,34 @@
+#include <Arduino.h>
+#include "buzzer_handler.h"
+#include "config.h"
+
+BuzzerHandler::BuzzerHandler() : _buzzerPin(PIN_BUZZER) {
+
+}
+
+
+void BuzzerHandler::initBuzzer(){
+    pinMode(_buzzerPin, OUTPUT);
+    digitalWrite(_buzzerPin, LOW);
+
+}
+
+void BuzzerHandler::alertLeak(){
+    for(int i = 0; i <3; i++){
+        digitalWrite(_buzzerPin, HIGH);
+        vTaskDelay(200 / portTICK_PERIOD_MS);
+        digitalWrite(_buzzerPin, LOW);
+        vTaskDelay(200 / portTICK_PERIOD_MS);
+    }
+}
+
+
+void BuzzerHandler::alertBlockage(){
+    for(int i = 0; i <2; i++){
+        digitalWrite(_buzzerPin, HIGH);
+        vTaskDelay(200 / portTICK_PERIOD_MS);
+        digitalWrite(_buzzerPin, LOW);
+        vTaskDelay(200 / portTICK_PERIOD_MS);
+    }
+}
+
