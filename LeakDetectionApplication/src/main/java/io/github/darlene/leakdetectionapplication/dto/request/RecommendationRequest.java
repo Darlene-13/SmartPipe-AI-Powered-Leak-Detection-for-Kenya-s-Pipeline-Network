56199583753e1e4511364e53218bbc36fd38a881
Darlene-13 <darlenewendie@gmail.com>
2026-05-15 08:47:27 +0300

    fixed imports

--- LeakDetectionApplication/src/main/java/io/github/darlene/leakdetectionapplication/dto/request/RecommendationRequest.java
+++ LeakDetectionApplication/src/main/java/io/github/darlene/leakdetectionapplication/dto/request/RecommendationRequest.java
@@ -1,5 +1,13 @@
+package io.github.darlene.leakdetectionapplication.dto.request;
+
+import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import java.util.Map;
+
 @Data
 public class RecommendationRequest {
+
     @NotBlank
     private String pipelineSegment;   // e.g. "A-B", "B-C"
 
