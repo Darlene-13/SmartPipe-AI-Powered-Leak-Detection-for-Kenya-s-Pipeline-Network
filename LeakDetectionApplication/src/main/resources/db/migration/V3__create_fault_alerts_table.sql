9d6c261edb1626ea161a78ca541eae86adcd34d1
Darlene-13 <darlenewendie@gmail.com>
2026-05-21 16:19:18 +0300

    fixed sql table creation

--- LeakDetectionApplication/src/main/resources/db/migration/V3__create_fault_alerts_table.sql
+++ LeakDetectionApplication/src/main/resources/db/migration/V3__create_fault_alerts_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTSfault_alerts (
+CREATE TABLE IF NOT EXISTS fault_alerts (
                               id                   BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
                               sensor_reading_id    BIGINT           NOT NULL,
                               sensor_reading_time  TIMESTAMPTZ      NOT NULL,
