9333adf3abafc13866c04a1e5575e17258e99aaf
Darlene-13 <darlenewendie@gmail.com>
2026-05-30 15:34:58 +0300

    modified springboot endpoint

--- LeakDetectionApplication/src/main/java/io/github/darlene/leakdetectionapplication/security/SecurityConfig.java
+++ LeakDetectionApplication/src/main/java/io/github/darlene/leakdetectionapplication/security/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig {
                 "https://ai-pipeline-leak-detection.onrender.com",
                 "http://localhost:3000",
                 "http://localhost:5173",
-                "http://leak-detection-backend-env.eba-fmxixjwp.us-east-1.elasticbeanstalk.com",
+                "https://leak-detection-backend-env.eba-fmxixjwp.us-east-1.elasticbeanstalk.com",
                 "https://main.d1cp8t2c10axin.amplifyapp.com",
                 "https://ai-pipeline-leak-detection-dashboard.onrender.com"
         ));
