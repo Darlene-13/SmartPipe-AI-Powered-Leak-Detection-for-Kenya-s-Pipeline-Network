df81115c98e78b7cd54db2bafa4cd3fc01fd72d2
Darlene-13 <darlenenasimiyu@gmail.com>
2026-06-05 06:19:10 +0300

    changed base url

--- dashboard/src/lib/api.ts
+++ dashboard/src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-export const BASE_URL = "https://leak-detection-backend-env.eba-fmxixjwp.us-east-1.elasticbeanstalk.com";
+export const BASE_URL = "https://relax-volumes-lyrics.ngrok-free.dev";
 
 export const api = axios.create({
     baseURL: BASE_URL,
